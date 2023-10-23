a74b5842008a3aeafc6262eab59ea9a35f25ae50
SaiVeerendra9 <prathipati.s@northeastern.edu>
2023-10-22 21:15:24 -0400

    added rds and security groups

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const internetGateway = require('./src/internetGateway');
 const routeTable = require("./src/routeTable");
 const securityGroup = require("./src/securityGroups");
 const ec2Instance = require("./src/ec2");
+const rds = require('./src/rds');
 
 const pulumi = require('@pulumi/pulumi');
 const config = new pulumi.Config();
@@ -13,6 +14,7 @@ create = async (name) => {
     const myvpc = vpc.createVpc(name);
     await subnet.createPublicSubnets(myvpc.id, name);
     await subnet.createPrivateSubnets(myvpc.id, name);
+    const privateSubnetGroup = await subnet.createPrivateSubnetsGroup(name, subnet.privateSubnets);
     const igw = internetGateway.createIG(name);
     internetGateway.attachVpc(myvpc.id, igw.id, name);
     const publicRT = routeTable.createPublicRouteTable(myvpc.id, name);
@@ -20,8 +22,11 @@ create = async (name) => {
     routeTable.associatePublicSubnets(subnet.publicSubnets, publicRT.id, name);
     const privateRT = routeTable.createPrivateRouteTable(myvpc.id, name);
     routeTable.associatePrivateSubnets(subnet.privateSubnets, privateRT.id, name);
-    const sgId = securityGroup.createApplicationSecurityGroup(myvpc.id);
-    const ec2 = ec2Instance.createEc2(name, sgId, subnet.publicSubnets[0].id);
+    const appsgId = securityGroup.createApplicationSecurityGroup(myvpc.id);
+    const dbsgId = securityGroup.createDbSecurityGroup(name, myvpc.id, appsgId);
+    const rdsParameterGroup = rds.createRdsParameterGroup();
+    const myRds = await rds.createRdsInstance(name, rdsParameterGroup, dbsgId, privateSubnetGroup); 
+    const ec2 = ec2Instance.createEc2(name, appsgId, subnet.publicSubnets[0].id, myRds);
 }
 
 create(config.get("name"));
--- src/ec2.js
+++ src/ec2.js
@@ -31,4 +31,35 @@ ec2.createEc2 = (name, securityGroupId, subnetId) => {
     return ec2Instance.id;
 }
 
+ec2.createEc2 = (name, securityGroupId, subnetId, rds) => {
+    // const rdsEndpoint = rds.endpoint.apply(endpoint => `PGHOST=${endpoint}`.toString());
+    const ec2Instance = new aws.ec2.Instance(`${name}-ec2-instance`, {
+        ami: amiId,
+        instanceType: config.get("instanceType"),
+        vpcSecurityGroupIds: [securityGroupId],
+        subnetId: subnetId,
+        rootBlockDevice: {
+            volumeSize: config.getNumber("volumeSize"),
+            volumeType: config.get("volumeType"),
+            deleteOnTermination: true,
+        },
+        associatePublicIpAddress: true,
+        keyName: publicKeyName,
+        tags: {
+            Name: `${name}-ec2-instance`,
+        },
+        disableApiTermination: false,
+        userData: pulumi.interpolate`#!/bin/bash
+        sudo yum update -y
+        echo "PGHOST=${rds.address}" >> /etc/environment
+        echo "PGUSER=${rds.username}" >> /etc/environment
+        echo "PGPASSWORD=${rds.password}" >> /etc/environment
+        echo "PGDATABASE=${rds.dbName}" >> /etc/environment
+        echo "PGPORT=${rds.port}" >> /etc/environment
+        `
+
+    });
+    return ec2Instance.id;
+}
+
 module.exports = ec2;
